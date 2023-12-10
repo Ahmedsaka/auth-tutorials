47caae34a6be658989da8e80d79d7660a2b06ec8
Ahmed Saka <ahmedsaka91@gmail.com>
2023-12-10 20:42:18 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,12 @@
+
+
+module.exports.db = {
+    host: "ahmedsaka.local",
+    port: 5432,
+    user:"postgres",
+    password : "postgres",
+    database : "postgres",
+    max: 20,
+    connectionTimeoutMillis : 0,
+    idleTimeoutMillis: 0
+}
